0b21ef644b26a24850338b8ed100537d360c7c48
Nuno Lopes <1251327@isep.ipp.pt>
2025-11-20 10:55:39 +0000

    Update projeto.java

--- projeto.java
+++ projeto.java
@@ -3,13 +3,29 @@ public class projeto {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
 
+        
         int numPessoas = input.nextInt();
         int numDias = input.nextInt();
+
+        final int LOW_MOOD = 2;
+        final int LIMITE_SUP_ESCALA = 5;
+        final int LIMITE_INF_ESCALA = 1;
 
         int[][] moodMap = fazerMoodMap(numPessoas, numDias);
         float[] mediaMoodDia = calcMedia(numDias, numPessoas, moodMap);
         float[] mediaMoodPessoa = calcMedia(numPessoas, numDias, moodMap);
         diasMoodMaximo(mediaMoodDia, numDias);
+        diasMauHumorConsectutivos(moodMap, numPessoas, numDias, LOW_MOOD);
+    }
+
+    public static int determinarMaximoMatrizInt(int[][] mapa, int linha, int dias){
+        int maximo  = 0;
+        for(int colunas = 0; colunas < dias; colunas++){
+            if(maximo< mapa[linha][colunas]){
+                maximo = mapa[linha][colunas];
+            }
+        }
+        return maximo;
     }
 
 // a) 
@@ -59,5 +75,41 @@ public class projeto {
         }
         System.out.printf("%n");
     }
+
+// g)
+    public static void diasMauHumorConsectutivos(int[][] mapa, int pessoas, int dias, int moodMin){
+
+        for(int linhas = 0; linhas < pessoas; linhas ++){
+            int diasHumorBaixo = 0;
+            for(int colunas = 0;  colunas < dias ; colunas++){
+                if(mapa[linhas][colunas] < moodMin){
+                    diasHumorBaixo++;
+                }else{
+                    diasHumorBaixo = 0;
+                }
+            }
+
+            if(diasHumorBaixo != 0 ){
+                System.out.printf("Person #%d : %d consecutive days%n", linhas, diasHumorBaixo);
+            }
+
+        }
+    }
+
+    public static void matrizGrafico()
+
+
+    public static void fazerGrafico(int dias, final int ESCALA, int[][] mapa){
+        for(int x = ESCALA; x < 0; x--){
+            for(int espacos = dias; espacos < 0; espacos--){
+
+            }
+        }
+
+
+    }
+
+
+
 
 }
